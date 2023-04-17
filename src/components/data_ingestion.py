453542ead32655181fcd4f081777df1ec3a48bf9
anil1415 <anilreddyk70@gmail.com>
2023-04-17 16:14:10 -0600

    pickle file created_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -34,8 +34,8 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path, index= False)
             logging.info('train_test_split the data')
             train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path, header= True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index = False)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False, header= True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index = False, header = True)
 
             logging.info('Ingestion of data is completed')
 
--- src/utils.py
+++ src/utils.py
@@ -13,10 +13,11 @@ def save_object(file_path, obj):
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, 'wb') as file_obj:
             pickle.dump(obj, file_obj)
-    except Exception as e:
 
+    except Exception as e:
         raise CustomException(e,sys)
     
 
